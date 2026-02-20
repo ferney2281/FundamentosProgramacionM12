21d14124182d93e95e4f333817ded18880aa4bb5
ferney2281 <ferney.chica@upb.edu.co>
2026-02-20 12:38:11 -0500

    Condicionales Simples
    Condicionales simples

---
+++ 2.CondicionalesSimples/2.CondicionalesSimples/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2.CondicionalesSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1.Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo supera los 3000 pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos.*/
+            /*string nombre="";
+            float sueldo = 0;
+            Console.WriteLine("Ingrese el nombre del usuario");
+            nombre = Console.ReadLine();//Lo que llega desde la consola, es de tipo string
+            Console.WriteLine("Ingrese el sueldo del usuario");
+            sueldo= Convert.ToSingle(Console.ReadLine());
+
+            if (sueldo>=3000)
+            {
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+            }*/
+
+            /*2.Crea un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18, se debe mostrar el siguiente mensaje: “Bienvenido a mi sitio web”. */
+        }
+    }
+}
