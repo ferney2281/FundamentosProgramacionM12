f9b0fa30d93b441d22b593fe8dc57682310799f2
Ferney Chica <ferney.chica@upb.edu.co>
2026-02-20 13:27:49 -0500

    Condicionales dobles, Prueba escritorio

---
+++ 3. CondicionalesDoblesCompuestos/3. CondicionalesDoblesCompuestos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace _3.CondicionalesDoblesCompuestos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un algoritmo que permita ingresar el nombre y el sueldo de una persona, si el sueldo supera los 3000 pesos mostrar el mensaje en pantalla con el nombre de la persona, indicando que debe abonar impuestos. De lo contrario mostrar el mensaje “Persona exenta de impuestos”.*/
+            string nombre = "";
+            float sueldo = 0;
+            Console.WriteLine("Ingrese el nombre del usuario");
+            nombre=Console.ReadLine();
+            Console.WriteLine("Ingrese el valor del sueldo");
+            sueldo= Single.Parse( Console.ReadLine() );
+
+            if (sueldo>=3000)
+            {
+                //Si la condición es Verdadera
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+            }
+            else
+            {
+                //Si la condición es Falsa
+                Console.WriteLine("La persona está exenta de impuestos");
+            }
+        }
+    }
+}
