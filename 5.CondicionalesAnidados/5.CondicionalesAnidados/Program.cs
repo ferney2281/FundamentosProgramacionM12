86ec3e12f6fa58912323b68ac7be26457d01defe
ferney2281 <ferney.chica@upb.edu.co>
2026-02-25 13:31:29 -0500

    Condicional Anidados
    Condicional Anidados

---
+++ 5.CondicionalesAnidados/5.CondicionalesAnidados/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _5.CondicionalesAnidados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Diseñar un algoritmo que lea tres números A, B, C y visualice en pantalla el valor más grande. El usuario debe ingresar tres valores diferentes.*/
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+
+            Console.WriteLine("Ingrese 3 valores diferentes");
+            Console.WriteLine("Ingrese el valor del número 1");
+            num1= Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del número 2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del número 3");
+            num3 = Int32.Parse(Console.ReadLine());
+
+            if (num1>num2)
+            {
+                if (num1>num3)
+                {
+                    Console.WriteLine($"El número mayor es: {num1}");
+                }
+                else
+                {
+                    Console.WriteLine($"El número mayor es: {num3}");
+                }
+            }
+            else
+            {
+                if (num2 > num3)
+                {
+                    Console.WriteLine($"El número mayor es: {num2}");
+                }
+                else
+                {
+                    Console.WriteLine($"El número mayor es: {num3}");
+                }
+            }
+        }
+    }
+}
